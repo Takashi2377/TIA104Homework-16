106a8b358256e799107bdb23d4dae16195bce250
Yuetin <aka05068@gmail.com>
2024-10-19 13:08:18 +0800

    upload hw7_12

---
+++ src/hw7/Hw7_1.java
@@ -0,0 +1,34 @@
+package hw7;
+
+import java.io.*;
+
+public class Hw7_1 {
+	public static void main(String[] args) throws Exception {
+		File sample = new File("Sample.txt");
+		long length = sample.length();
+		FileReader fr = new FileReader(sample);
+		
+		int ch = 0;
+		while(fr.read() != -1) {
+			ch++;
+		}
+		
+		
+		FileReader fr2 = new FileReader(sample);
+		BufferedReader br = new BufferedReader(fr2);
+		
+		int ln = 0;
+		while(br.readLine() != null) {
+			ln++;
+		}
+		
+		System.out.println("Sample.txt檔案共有" + length + 
+						   "個位元組, " + ch + 
+						   "個字元, " + ln + "列資料");
+		
+		br.close();
+		fr.close();
+		fr2.close();
+		
+	}
+}
---
+++ src/hw7/Hw7_2.java
@@ -0,0 +1,25 @@
+package hw7;
+
+import java.io.*;
+
+public class Hw7_2 {
+	public static void main(String[] args) throws Exception {
+		
+		FileOutputStream fos = new FileOutputStream("Data.txt", true);
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		PrintStream ps = new PrintStream(bos);
+
+		for(int n = 0; n <= 9; n++) {
+			int num;
+			if((num = (int)Math.floor(Math.random() * 1000 + 1)) > 0) {
+				ps.print(num + "\t");
+			}else {
+				n--;
+			}
+		}
+		ps.println();
+		ps.close();
+		bos.close();
+		fos.close();
+	}
+}
